71a79c290dac3c44bb8ae2b62f53ea1bacf92dac
poyrazogluyigit <poyrazogluyigit@gmail.com>
2024-03-06 22:53:41 +0300

    modified as generators

--- properties/bounded_recurrence.py
+++ properties/bounded_recurrence.py
@@ -4,19 +4,7 @@ from collections import namedtuple
 TimedData = namedtuple('TimedData', ["time", 'data'])
 
 
-class bounded_recurrence_globally(object):
-
-    # @staticmethod
-    # def generate_future_formula(lower, upper):
-    # 	return "always( eventually[{a},{b}](p))".format(a=lower, b=upper)
-
-    @staticmethod
-    def generate_past_formula(upper_bound):
-        return "historically(once[:{b}]({{p}}))".format(b=upper_bound)
-
-    @staticmethod
-    def generate_future_formula(upper_bound):
-        return "always(eventually[:{b}]({{p}}))".format(b=upper_bound)
+class bounded_recurrence_globally:
 
     @staticmethod
     def generate_trace(upper_bound, duration=1000, failing_end=True):
@@ -24,9 +12,8 @@ class bounded_recurrence_globally(object):
         Data = namedtuple('Data', ['p'])
 
         time = 0
-        rows = []
 
-        rows += [TimedData(time=time, data=Data(p=True))]
+        yield [TimedData(time=time, data=Data(p=True))]
         time += 1
 
         while time < duration:
@@ -35,32 +22,22 @@ class bounded_recurrence_globally(object):
             k = random.randint(1, upper_bound)
 
             for j in range(k-1):
-                rows += [TimedData(time=time, data=Data(p=False))]
+                yield [TimedData(time=time, data=Data(p=False))]
                 time += 1
 
-            rows += [TimedData(time=time, data=Data(p=True))]
+            yield [TimedData(time=time, data=Data(p=True))]
             time += 1
 
         if failing_end:
             for j in range(upper_bound+1):
-                rows += [TimedData(time=time, data=Data(p=False))]
+                yield [TimedData(time=time, data=Data(p=False))]
                 time += 1
 
-            # rows += [TimedData(time=time, data=Data(p=True))]
+            # yield [TimedData(time=time, data=Data(p=True))]
             # time += 1
-
-        return rows
 
 
 class bounded_recurrence_between_q_and_r:
-
-    @staticmethod
-    def generate_past_formula(upper_bound):
-        return "historically(({{r}} && !{{q}} && once {{q}}) -> ((once[:{b}]({{p}} or {{q}})) since {{q}}))".format(b=upper_bound)
-
-    @staticmethod
-    def generate_future_formula(upper_bound):
-        return "always(({{q}} && !{{r}} && eventually {{r}}) -> ((eventually[:{b}]({{p}} or {{r}})) until {{r}}))".format(b=upper_bound)
 
     @staticmethod
     def generate_trace(upper_bound, duration=1000, min_recur=True, max_recur=7, failing_end=True):
@@ -68,11 +45,10 @@ class bounded_recurrence_between_q_and_r:
         Data = namedtuple('Data', ['q', 'p', 'r'])
 
         time = 0
-        rows = []
 
         while time < duration:
 
-            rows += [TimedData(time=time, data=Data(q=True, p=False, r=False))]
+            yield [TimedData(time=time, data=Data(q=True, p=False, r=False))]
             time += 1
 
             for i in range(0, random.randint(min_recur, max_recur)):
@@ -81,34 +57,33 @@ class bounded_recurrence_between_q_and_r:
                 k = random.randint(1, upper_bound)
 
                 for j in range(k-1):
-                    rows += [TimedData(time=time,
+                    yield [TimedData(time=time,
                                        data=Data(q=False, p=False, r=False))]
                     time += 1
 
-                rows += [TimedData(time=time,
+                yield [TimedData(time=time,
                                    data=Data(q=False, p=True, r=False))]
                 time += 1
 
             for j in range(k-1):
-                rows += [TimedData(time=time,
+                yield [TimedData(time=time,
                                    data=Data(q=False, p=False, r=False))]
                 time += 1
 
-            rows += [TimedData(time=time, data=Data(q=False, p=False, r=True))]
+            yield [TimedData(time=time, data=Data(q=False, p=False, r=True))]
             time += 1
 
             for j in range(k-1):
-                rows += [TimedData(time=time,
+                yield [TimedData(time=time,
                                    data=Data(q=False, p=False, r=False))]
                 time += 1
 
         if failing_end:
-            rows += [TimedData(time=time, data=Data(q=True, p=False, r=False))]
+            yield [TimedData(time=time, data=Data(q=True, p=False, r=False))]
             time += 1
             for j in range(upper_bound+1):
-                rows += [TimedData(time=time,
+                yield [TimedData(time=time,
                                    data=Data(q=False, p=False, r=False))]
                 time += 1
-            rows += [TimedData(time=time, data=Data(q=False, p=False, r=True))]
+            yield [TimedData(time=time, data=Data(q=False, p=False, r=True))]
             time += 1
-        return rows
